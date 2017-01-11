8ffa3473dac7dc9c51bf756489e0ccfff57d0f9c
Joachim Bjørge <jb@ground.no>
2017-01-11 17:35:15 +0100

    Fixed bug with crashing when not finding previous versions to copy files from.

--- src/copyExistingFiles.js
+++ src/copyExistingFiles.js
@@ -1,8 +1,9 @@
 const copyFiles = require('./copyFiles');
+const Promise = require('bluebird');
 
 function copyExistingFiles(dataDir, versionFolderName, filesToCopy, destinationFolderName) {
 	if (!versionFolderName)
-		throw new Error('cordova-code-swap: No previous versions to copy files from.');
+		return Promise.reject(new Error('cordova-code-swap: No previous versions to copy files from.'));
 
 	const srcFolderPath = dataDir + versionFolderName + '/';
 	return copyFiles(srcFolderPath, filesToCopy, dataDir, destinationFolderName);
