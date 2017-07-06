fab8ce6db7f2dff9e7030de49d3f48f815fd90bf
Joachim Bjørge <jb@ground.no>
2017-07-06 09:52:51 +0200

    Added node to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "browser": true,
         "commonjs": true,
         "es6": true,
-        "mocha": true
+        "mocha": true,
+        "node": true
     },
     "globals": {
         "FileTransfer": true,
