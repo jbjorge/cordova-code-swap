445e1f496222d6d916e3becd7c287f9a5b3196f0
Joachim Bjørge <jb@ground.no>
2017-07-06 09:54:05 +0200

    Now runs tests in utils/ folder.

--- test/unit-tests.js
+++ test/unit-tests.js
@@ -4,5 +4,8 @@ const path = require('path');
 describe('Unit tests', () => {
 	context('src/', () => {
 		findTests({ rootPath: path.resolve(__dirname, '../src') });
-	})
+	});
+	context('utils/', () => {
+		findTests({ rootPath: path.resolve(__dirname, '../utils') });
+	});
 });
